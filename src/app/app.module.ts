820cd227a57b27247c15f4ecaebac77451d14282
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-06-09 09:09:14 -0700

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
+import { NewAnimalComponent } from './new-animal/new-animal.component';
+import { EditAnimalComponent } from './edit-animal/edit-animal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimalListComponent,
+    NewAnimalComponent,
+    EditAnimalComponent
   ],
   imports: [
     BrowserModule
